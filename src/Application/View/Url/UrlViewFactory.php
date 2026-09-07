cb94bd51bbca7847b90cbfba841d9fd0662938f2
Aleksey Vinnik <alexey.vinnik@simple-solution.biz>
2026-09-07 20:41:56 +0300

    Add public and api domains

--- src/Application/View/Url/UrlViewFactory.php
+++ src/Application/View/Url/UrlViewFactory.php
@@ -7,7 +7,7 @@ use Domain\Url\Entities\Url;
 final readonly class UrlViewFactory
 {
     public function __construct(
-        private string $defaultUri,
+        private string $publicUri,
     ) {}
 
     public function create(Url $url): UrlView
@@ -17,7 +17,7 @@ final readonly class UrlViewFactory
         $view->id = $url->getId()->toString();
         $view->code = $url->getCode();
         $view->target_url = $url->getTargetUrl();
-        $view->short_url = rtrim($this->defaultUri, '/').'/'.$url->getCode();
+        $view->short_url = rtrim($this->publicUri, '/').'/'.$url->getCode();
 
         return $view;
     }
