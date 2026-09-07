38b16aa31682b6a9ebdb917f583d6e92d2a0bc6c
Aleksey Vinnik <alexey.vinnik@simple-solution.biz>
2026-09-07 19:11:31 +0300

    Make click recording asynchronously

--- src/Infrastructure/MessageBus/CommandBus.php
+++ src/Infrastructure/MessageBus/CommandBus.php
@@ -4,25 +4,21 @@ namespace Infrastructure\MessageBus;
 
 use Domain\MessageBus\CommandBusInterface;
 use Symfony\Component\Messenger\Exception\HandlerFailedException;
-use Symfony\Component\Messenger\HandleTrait;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Throwable;
 
 final class CommandBus implements CommandBusInterface
 {
-    use HandleTrait;
-
     public function __construct(
-        readonly MessageBusInterface $commandBus,
+        private readonly MessageBusInterface $commandBus,
     ) {
-        $this->messageBus = $commandBus;
     }
 
     /** {@inheritDoc} */
     public function dispatch(object $command): void
     {
         try {
-            $this->handle($command);
+            $this->commandBus->dispatch($command);
         } catch (HandlerFailedException $handlerFailedException) {
             if ($handlerFailedException->getPrevious() instanceof Throwable) {
                 throw $handlerFailedException->getPrevious();
