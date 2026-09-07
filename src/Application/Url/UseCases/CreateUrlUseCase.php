8d497652801f6c374e5718e1e61ffcc8eda74d4e
Aleksey Vinnik <alexey.vinnik@simple-solution.biz>
2026-09-07 21:20:25 +0300

    Update `CreateUrlUseCase` attributes

--- src/Application/Url/UseCases/CreateUrlUseCase.php
+++ src/Application/Url/UseCases/CreateUrlUseCase.php
@@ -2,6 +2,8 @@
 
 namespace Application\Url\UseCases;
 
+use OpenApi\Attributes as OA;
+use Symfony\Component\Serializer\Annotation\Ignore;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -10,7 +12,14 @@ final class CreateUrlUseCase
     #[Assert\NotBlank(message: 'The target URL is required.')]
     #[Assert\Url(message: 'The target URL is not a valid URL.', requireTld: true)]
     #[Assert\Length(max: 254)]
+    #[OA\Property(
+        property: 'target_url',
+        description: 'The original, long URL to shorten.',
+        type: 'string',
+        example: 'https://example.com/some/very/long/path?with=query&params=here',
+    )]
     public string $targetUrl;
 
+    #[Ignore]
     public Uuid $id;
 }
