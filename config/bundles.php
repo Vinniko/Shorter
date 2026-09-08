b8e30c92e9bca3b6fd33f53096aaf2a3e731d005
Aleksey Vinnik <alexey.vinnik@simple-solution.biz>
2026-09-08 11:17:54 +0300

    Install nelmio bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
